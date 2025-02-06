6427ee06f2a0629d55678935e64bfac2b6ba9f14
Stefan Haun <mail@tuxathome.de>
2025-02-06 13:42:07 +0100

    Add files created by Spring Initializr
    See https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.2&packaging=jar&jvmVersion=21&groupId=com.penguineering.synctexng&artifactId=synctexng-rmq-server&name=synctexng-rmq-server&description=Next%20Generation%20SyncText%20Server%20using%20RabbitMQ&packageName=com.penguineering.synctexng.synctexng-rmq-server&dependencies=lombok,webflux,amqp,actuator

---
+++ src/main/java/com/penguineering/synctexng/synctexng_rmq_server/SynctexngRmqServerApplication.java
@@ -0,0 +1,13 @@
+package com.penguineering.synctexng.synctexng_rmq_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynctexngRmqServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynctexngRmqServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/penguineering/synctexng/synctexng_rmq_server/SynctexngRmqServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penguineering.synctexng.synctexng_rmq_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynctexngRmqServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
